cb193af1aa8fe265a1e48074c7f27fe18311095d
zuoteng <lancelotnemoj@outlook.com>
2020-06-21 15:12:17 +0800

    first commit

---
+++ precache-manifest.1055a293c0509c8bd59e2ddeb1d63bdc.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9d05dcf9a338d7765c8126dc666fe811",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e0ad2be4f64d01194a34",
+    "url": "/static/css/2.eef80f9b.chunk.css"
+  },
+  {
+    "revision": "81f78889b0aace89ea95",
+    "url": "/static/css/main.5a7de7e3.chunk.css"
+  },
+  {
+    "revision": "e0ad2be4f64d01194a34",
+    "url": "/static/js/2.dcc2be62.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.dcc2be62.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "81f78889b0aace89ea95",
+    "url": "/static/js/main.bb62f5f5.chunk.js"
+  },
+  {
+    "revision": "3e07a6c76033c034d5f5",
+    "url": "/static/js/runtime-main.9bb58a9c.js"
+  },
+  {
+    "revision": "ce3014a79aa11b9642789484cb8afc7f",
+    "url": "/static/media/sign.ce3014a7.jpg"
+  },
+  {
+    "revision": "f4aa78226f17235118b7efcec4c818c5",
+    "url": "/static/media/zyl.f4aa7822.jpg"
+  }
+]);
